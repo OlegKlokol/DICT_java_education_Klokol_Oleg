c0486b6e0628cfc8a9e777e9a3e399039ee52131
OlegKlokol <o.v.klokol@student.khai.edu>
2021-12-21 22:34:10 +0200

    Resolved conflict

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,10 @@ public class Test {
 =======
         System.out.println("education");
         //new string
+<<<<<<< HEAD
 >>>>>>> bdf0521 (test.java added to feature_branch with change on string #2)
+=======
+        System.out.println("DICT");
+>>>>>>> 903969d (test.java added to feature_branch with change on string #3)
     }
 }
