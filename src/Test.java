7f9178e388184cc0ceefe29a6710feec3678c99b
OlegKlokol <o.v.klokol@student.khai.edu>
2021-12-21 22:32:25 +0200

    Resolved conflict

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,10 @@
 public class Test {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("Java education");
+=======
+        System.out.println("education");
+        //new string
+>>>>>>> bdf0521 (test.java added to feature_branch with change on string #2)
     }
 }
